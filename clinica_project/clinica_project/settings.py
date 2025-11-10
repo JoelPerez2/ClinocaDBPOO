8f3e016aa5dd1ce168a448f215012408ba950b0e
JoelPerez2 <cp0306032025@unab.edu.sv>
2025-11-10 17:37:30 -0600

    Actualizacion del settings.py

--- clinica_project/clinica_project/settings.py
+++ clinica_project/clinica_project/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['clinocadbpoo.onrender.com', 'localhost', '127.0.0.1']
 
 
 
